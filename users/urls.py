ce023b4653e63644a5885fad7948466481dd2210
Mohit Prajapat <mohitdevelopment2001@gmail.com>
2024-04-27 21:42:26 +0530

    [Updated] user model extended

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("accounts/profile/", views.Profile.as_view(), name="home"),
+    path("register/", views.Register.as_view(), name="register"),
+    path("login/", views.Login.as_view(), name="login"),
+    path("logout/", views.Login.as_view(), name="logout"),
+    
+]
