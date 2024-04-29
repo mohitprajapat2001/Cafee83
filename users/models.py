97f046b4e7a8b3442ccd6c16f7823afdf22688e9
Mohit Prajapat <mohitdevelopment2001@gmail.com>
2024-04-29 13:35:58 +0530

    [Added] Profile Section User Data Edit Modal || [Updated] User Modal Profile Field with Default Profile Image

--- users/models.py
+++ users/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 class Customer(AbstractUser):
-    profile= models.ImageField(upload_to="Customer_Profile", null=True, blank=True)
-    age= models.IntegerField(null=True, blank=True)
-    address= models.CharField(max_length=50, null=True, blank=True)
+    profile = models.ImageField(
+        upload_to="Customer_Profile",
+        default="default/profile2.png",
+        null=True,
+        blank=True,
+    )
+    age = models.IntegerField(null=True, blank=True)
+    address = models.CharField(max_length=50, null=True, blank=True)
     phone = models.IntegerField(default=000000000, null=True, blank=True)
-    
+
     def __str__(self):
         return self.username
