5c0e992ce410562ffe54235b968e72882f5c5625
Mohit Prajapat <mohitdevelopment2001@gmail.com>
2024-05-05 15:51:48 +0530

    [Updated] Customer Model Profile Remove Default Image

--- users/models.py
+++ users/models.py
@@ -1,15 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Customer(AbstractUser):
-    profile = models.ImageField(
-        upload_to="Customer_Profile",
-        default="default/profile2.png",
-        null=True,
-        blank=True
-    )
+    profile = models.ImageField(upload_to="Customer_Profile", null=True, blank=True)
     age = models.IntegerField(null=True, blank=True)
     address = models.CharField(max_length=50, null=True, blank=True)
     phone = PhoneNumberField(region="IN")
